30ed734e6c2a7cd846f2a996e53ccc2acaa71a27
“TienDung1706“ <tiendung17062k4@gmail.com>
2025-10-09 13:31:18 +0700

    Bỏ header thừa ở trang freeentrytest

--- src/pages/FreeEntryTest.jsx
+++ src/pages/FreeEntryTest.jsx
@@ -26,8 +26,6 @@ export default function FreeEntryTest() {
 
   return (
     <>
-      <HeaderToeicHome />
-
       <div className="max-w-4xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-extrabold text-gray-900 mb-8 text-center">
           Chọn Bài Thi TOEIC
